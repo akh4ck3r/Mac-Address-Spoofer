4b840d538c43f490703976ea7a1e2bfd7ca8e568
akh4ck3r <109330522+akh4ck3r@users.noreply.github.com>
2025-12-03 12:28:50 +0530

    Add files via upload

---
+++ macspoofer.py
@@ -0,0 +1,31 @@
+import subprocess
+import re
+
+#subprocess.run(["ifconfig"])
+
+networkInterfacname = input("Enter Network InterFace Name: ")
+
+#mac = input("Enter your MAC address: ")
+def validate_mac(user_input):
+    # Regex: 6 groups of 2 alphanumeric chars separated by 5 colons
+    pattern = r'^[A-Za-z0-9]{2}(:[A-Za-z0-9]{2}){5}$'
+
+    if re.match(pattern, user_input):
+        return True
+    else:
+        return False
+
+
+# ----- Main Program -----
+mac = input("Enter new MAC Address: ")
+
+if validate_mac(mac):
+    print("MAC Address string format is Valid!")
+else:
+    print("MAC Address string format is Invalid!")
+
+subprocess.run(["macchanger", "-m", mac, networkInterfacname])
+subprocess.run(["macchanger", "-m", mac, networkInterfacname])
+subprocess.run(["macchanger", "-m", mac, networkInterfacname])
+
+print("MAC address has been changed!")
